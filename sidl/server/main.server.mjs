9e16d19bd6dc331097946029d3d4d9aa5a4dbc8d
Danilo Santos <dosv01@users.noreply.github.com>
2024-03-20 17:04:37 -0300

    .

--- sidl/server/main.server.mjs
+++ sidl/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LRXVQP5W.mjs";import"./chunk-CMK6BTVX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-T3EH3GGF.mjs";import"./chunk-CMK6BTVX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
