63fe70b78aec23bd0383d3eeaa9f9888ae765d84
Danilo Santos <dosv01@users.noreply.github.com>
2024-03-20 15:21:49 -0300

    alterações para deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4E2WPJRR.mjs";import"./chunk-CMK6BTVX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AOE33QHK.mjs";import"./chunk-CMK6BTVX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
