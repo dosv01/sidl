2070122a294d7d8a6f7425e5fbde0022945a1ded
Danilo Santos <dosv01@users.noreply.github.com>
2024-04-10 21:22:23 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'sidl';
-  private _jsonURL = 'assets/perguntas_sidl.json';
+  private _jsonURL = 'assets/perguntas_sidl_2.json';
   perguntas: any = [];
   totalPerguntas = 0;
   values = Object.values;
